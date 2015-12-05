8e39997080b246dbc31b178d0aeb34c7b59ba744
Martin Erzberger <martin@erzberger.ch>
2015-12-05 12:16:53 +0100

    Improve in-code documentation

--- apa102.py
+++ apa102.py
@@ -12,33 +12,43 @@ Public methods are:
  - show
  - clearStrip
  - cleanup
- 
+
 Helper methods for color manipulation are:
  - combineColor
  - wheel
- 
+
 The rest of the methods are used internally and should not be used by the user of the library.
 
-Very brief overview of APA102: An APA102 LED is addressed with SPI. The bits are shifted in one by one, 
+Very brief overview of APA102: An APA102 LED is addressed with SPI. The bits are shifted in one by one,
 starting with the least significant bit.
 
-An LED usually just copies everything that is sent to it via data-in to its data-out. While doing this, it
-remembers its own color and keeps glowing with that color as long as there is power. If the color of a
-particular LED must be updated, then (at least) 32 bits of zeroes must be sent to data-in. The LED then
-accepts the next real 32 bit LED frame (with color information) as its new color setting.
+An LED usually just forwards everything that is sent to its data-in to data-out. While doing this, it
+remembers its own color and keeps glowing with that color as long as there is power.
 
-After having received the 32 bit color frame, the LED changes color, and then goes on to just copying
+An LED can be switched to not forward the data, but instead use the data to change it's own color.
+This is done by sending (at least) 32 bits of zeroes to data-in. The LED then accepts the next
+correct 32 bit LED frame (with color information) as its new color setting.
+
+After having received the 32 bit color frame, the LED changes color, and then resumes to just copying
 data-in to data-out.
 
-The really clever bit is this: While receiving the 32 bit LED frame, the LED sends all zeroes on its
-data-out line. So the next LED down the strip receives a "color change" frame automatically. Since the
-next 32 bits are then just copied by LED 1 to LED 2, LED 2 now updates its color, and preps LED 3 at
-the same time.
+The really clever bit is this: While receiving the 32 bit LED frame, the LED sends zeroes on its
+data-out line. Because a color frame is 32 bits, the LED sends 32 bits of zeroes to the next LED.
+As we have seen above, this means that the next LED is now ready to accept a color frame and
+update its color.
 
 So that's really the entire protocol:
-- Start LED 1 with 32 bits of zeroes
-- Send one 32 bit color frame per LED on the strip to data-in of the first LED
+- Start by sending 32 bits of zeroes. This prepares LED 1 to update its color.
+- Send color information one by one, starting with the color for LED 1, then LED 2 etc.
 - Finish off by cycling the clock line a few times to get all data to the very last LED on the strip
+
+The last step is necessary, because each LED delays forwarding the data a bit. Imagine ten people in
+a row. When you yell the last color information, i.e. the one for person ten, to the first person in
+the line, then you are not finished yet. Person one has to turn around and yell it to person 2, and
+so on. So it takes ten additional "dummy" cycles until person ten knows the color. When you look closer,
+you will see that not even person 9 knows the color yet. This information is still with person 2.
+Essentially the driver sends additional zeroes to LED 1 as long as it takes for the last color frame
+to make it down the line to the last LED.
 """
 class APA102:
     def __init__(self, numLEDs, globalBrightness = 31): # The number of LEDs in the Strip
@@ -52,30 +62,32 @@ class APA102:
         self.spi = spidev.SpiDev()  # Init the SPI device
         self.spi.open(0, 1)  # Open SPI port 0, slave device (CS)  1
         self.spi.max_speed_hz=8000000 # Up the speed a bit, so that the LEDs are painted faster
-    
+
     """
+    void clockStartFrame()
     This method clocks out a start frame, telling the receiving LED that it must update its own color now.
     """
     def clockStartFrame(self):
         _ = self.spi.xfer2([0x00, 0x00, 0x00, 0x00])  # Start frame, 32 zero bits
-    
+
     """
-    The end frame is not really a data package. Its purpose is to trigger additional clock pulses. The clock pulses
-    are required because of the way SPI works. In SPI, the first step is to set a stable data signal. Then, the
-    clock signal is raised. This triggers the receiver to read the data line.
-    This works fine for one LED. But the second LED has a problem with this. As soon as LED one reads data on its
-    data-in, it should prepare data on data-out. But it can't do this while the clock line is "high". So LED one
-    will invert the clock signal.
-    When the clock-in of LED one is high, LED one sets its clock-out to low.
-    It readies its data-out, and as soon as the LED one input clock is lowered in order to prepare the next input for
-    LED one, the clock-out of LED one is raised, and LED 2 can read the data line. 
-    This goes on like this through the entire strip. Each LED delays its data-out by 1/2 clock cycle by inverting the 
-    clock signal. At the end of the strip, we are numLEDs/2 clock cycles behind. Usually this means that the last LED did not
-    yet receive all of its data (32 bits is a full LED data frame, so if the strip has 64 LEDs or less, then only the last LED
-    is affected).
+    void clockEndFrame()
+    As explained above, dummy data must be sent after the last real color information so that all of the data
+    can reach its destination down the line.
+    The delay is not as bad as with the human example above. It is only 1/2 bit per LED. This is because the
+    SPI clock line needs to be inverted.
+
+    Say a bit is ready on the SPI data line. The sender communicates this by toggling the clock line. The bit
+    is read by the LED, and immediately forwarded to the output data line. When the clock goes down again
+    on the input side, the LED will toggle the clock up on the output to tell the next LED that the bit is ready.
+
+    After one LED the clock is inverted, and after two LEDs it is in sync again, but one cycle behind. Therefore,
+    for every two LEDs, one bit of delay gets accumulated. For 300 LEDs, 150 additional bits must be fed to
+    the input of LED one so that the data can reach the last LED.
+
     Ultimately, we need to send additional numLEDs/2 arbitrary data bits, in order to trigger numLEDs/2 additional clock
     changes. This driver sends zeroes, which has the benefit of getting LED one partially or fully ready for the next update
-    to the strip. An optimized version of the driver could omit the "clockStartFrame" method if enough zeroes have 
+    to the strip. An optimized version of the driver could omit the "clockStartFrame" method if enough zeroes have
     been sent as part of "clockEndFrame".
     """
     def clockEndFrame(self):
@@ -83,13 +95,16 @@ class APA102:
             self.spi.xfer2([0x00])
 
     """
-    Sets the color for the entire strip to black, and immediately shows the result.
+    void clearStrip()
+    Sets the color for the entire strip to black, and immediately shows the result. The method does
+    not update the color buffer, it only clears the strip!
     """
     def clearStrip(self):
         self.spi.xfer2([self.ledstart, 0x00, 0x00, 0x00] * self.numLEDs)
         self.clockEndFrame() # ... and clock the end frame so that also the last LED(s) shut down.
 
     """
+    void setPixel(ledNum, red, green, blue)
     Sets the color of one pixel in the LED stripe. The changed pixel is not shown yet on the Stripe, it is only
     written to the pixel buffer. Colors are passed individually.
     """
@@ -103,8 +118,9 @@ class APA102:
         self.leds[startIndex+3] = red
         self.leds[startIndex+1] = green
         self.leds[startIndex+2] = blue
-        
+
     """
+    void setPixelRGB(ledNum,rgbColor)
     Sets the color of one pixel in the LED stripe. The changed pixel is not shown yet on the Stripe, it is only
     written to the pixel buffer. Colors are passed combined (3 bytes concatenated)
     """
@@ -112,27 +128,31 @@ class APA102:
         self.setPixel(ledNum, (rgbColor & 0xFF0000) >> 16, (rgbColor & 0x00FF00) >> 8, rgbColor & 0x0000FF)
 
     """
+    void show()
     Sends the content of the pixel buffer to the strip.
     Todo: More than 1024 LEDs requires more than one xfer operation.
     """
     def show(self):
         self.clockStartFrame()
-        self.spi.xfer2(self.leds) # SPI takes up to 4096 Integers. So we are fine for up to 1024 LEDs. 
+        self.spi.xfer2(self.leds) # SPI takes up to 4096 Integers. So we are fine for up to 1024 LEDs.
         self.clockEndFrame()
 
     """
+    void cleanup()
     This method should be called at the end of a program in order to release the SPI device
     """
     def cleanup(self):
         self.spi.close()  # ... SPI Port schliessen
-        
+
     """
-     Make one 3*8 byte color value
+    color combineColor(red,green,blue)
+    Make one 3*8 byte color value
     """
     def combineColor(self, red, green, blue):
         return (red << 16) + (green << 8) + blue
 
     """
+    color wheel(wheelPos)
     Get a color from a color wheel
     Green -> Red -> Blue -> Green
     """
